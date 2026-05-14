8e4e73d8d3be8b077cadf52eb9a7dd5f25314012
k1shor <k1shor@hotmail.com>
2026-05-14 15:57:36 +0545

    Update response message in root route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Node.js app is running");
+  res.send("Node.js app is running!!!");
 });
 
 app.get("/api/hi", (req, res) => {
