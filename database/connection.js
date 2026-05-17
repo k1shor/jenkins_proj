28218f004aac17692333994f4c250cabb1e1eae0
kishor munikar <k1shor@hotmail.com>
2026-05-17 13:12:43 +0545

    connection

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI || "mongodb://test:test1234@cluster0-shard-00-00.2aidv.mongodb.net:27017,cluster0-shard-00-01.2aidv.mongodb.net:27017,cluster0-shard-00-02.2aidv.mongodb.net:27017/devops3_4?ssl=true&replicaSet=atlas-f9tung-shard-0&authSource=admin&appName=Cluster0")
     .then(() => console.log('MongoDB connected'))
     .catch(error => console.log(error))
